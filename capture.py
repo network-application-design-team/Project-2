03c55a0bf016198ef110a245a6552d1ba23c50ad
Honaker <chris98@vt.edu>
2019-03-23 12:59:01 -0400

    basic

--- capture.py
+++ capture.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python
 
+import pika
+import sys
+import time
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='Squires', exchange_type='direct')
+channel.exchange_declare(exchange='Goodwin', exchange_type='direct')
+channel.exchange_declare(exchange='Library', exchange_type='direct')
+
+
+
+message = ' '.join(sys.argv[1:]) or "info:Hello World!"
+channel.basic_publish(exchange='Squires', routing_key='', body=message)
+connection.close()
