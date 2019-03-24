e40e9846ccc0f77939541956d3af014cb5a03cb9
Honaker <chris98@vt.edu>
2019-03-24 17:44:33 -0400

    fixed queue name

--- capture.py
+++ capture.py
@@ -9,7 +9,7 @@ def fetch_ip():
     return((([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close())\
       for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0])
 
-node = "192.168.0.13"
+node = "172.30.126.1"
 username = "Honaker"
 password = "buse"
 
@@ -24,10 +24,10 @@ channel.exchange_declare(exchange='Library', exchange_type='direct')
 
 
 
-message = ' '.join(sys.argv[1:]) or "info:Hello World!"
-channel.basic_publish(exchange='Squires', routing_key='Squires', body=message)
-channel.basic_publish(exchange='Goodwin', routing_key='Goodwin', body=message)
-channel.basic_publish(exchange='Library', routing_key='Library', body=message)
+message = "info:Hello World!"
+channel.basic_publish(exchange='Squires', routing_key='Food', body=message)
+#channel.basic_publish(exchange='Goodwin', routing_key='Goodwin', body=message)
+#channel.basic_publish(exchange='Library', routing_key='Library', body=message)
 
 
 connection.close()
