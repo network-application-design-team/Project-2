26cd02d007b63b9bda4cd79e6af7acbf3aaadd9d
Kishan Parikh <kjp2376@vt.edu>
2019-03-31 14:59:02 -0400

    Moved MongoDB into its own instance

--- capture.py
+++ capture.py
@@ -7,12 +7,73 @@ import pymongo
 import threading
 import subprocess
 import datetime
+
 # imports for twitter
 import tweepy
 import json
 import re
 import substring as substr
 import captureKeys
+import threading
+
+
+def mongoInsert(
+    action,
+    place,
+    subject,
+    message,
+    squiresRooms,
+    squiresFood,
+    squiresMeetings,
+    libraryNoise,
+    librarySeating,
+    libraryWishes,
+    goodwinAuditorium,
+    goodwinClassrooms,
+):
+    # Test Data
+    # action = "p"
+    # place = "Squires"
+    msgID = "20" + "$" + str(time.time())
+    # subject = "Rooms"
+    # message = "I like to be comfortable"
+    post = {
+        "Action": action,
+        "Place": place,
+        "MsgID": msgID,
+        "Subject": subject,
+        "Message": message,
+    }
+
+    if place is "Squires":
+        if subject is "Rooms":
+            post_ID = squiresRooms.insert_one(post).inserted_id
+            documentInserted = squiresRooms.find_one({"_id": post_ID})
+        elif subject is "Food":
+            post_ID = squiresFood.insert_one(post).inserted_id
+            documentInserted = squiresFood.find_one({"_id": post_ID})
+        elif subject is "Meetings":
+            post_ID = squiresMeetings.insert_one(post).inserted_id
+            documentInserted = squiresMeetings.find_one({"_id": post_ID})
+    elif place is "Library":
+        if subject is "Noise":
+            post_ID = libraryNoise.insert_one(post).inserted_id
+            documentInserted = libraryNoise.find_one({"_id": post_ID})
+        elif subject is "Seating":
+            post_ID = librarySeating.insert_one.inserted_id
+            documentInserted = librarySeating.find_one({"_id": post_ID})
+        elif subject is "Wishes":
+            post_ID = libraryWishes.insert_one(post).inserted_id
+            documentInserted = libraryWishes.find_one({"_id": post_ID})
+    elif place is "Goodwin":
+        if subject is "Classrooms":
+            post_ID = goodwinClassrooms.insert_one(post).inserted_id
+            documentInserted = goodwinClassrooms.find_one({"_id": post_ID})
+        elif subject is "Auditorium":
+            post_ID = goodwinAuditorium.insert_one(post).inserted_id
+            documentInserted = goodwinAuditorium.find_one({"_id": post_ID})
+    return
+
 
 # Twitter Section
 # Keys for twitter dev api
@@ -23,36 +84,38 @@ asecret = captureKeys.asecret
 # setting up authentication
 auth = tweepy.OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
-print('Connected to Twitter')
+print("Connected to Twitter")
 
 # Twitter class:
 class listener(tweepy.StreamListener):
     def on_data(self, data):
         all_data = json.loads(data)
         tweet = all_data["text"]
-        tweeter = tweet.replace('#ECE4564T20 ', '')
+        tweeter = tweet.replace("#ECE4564T20 ", "")
 
-        if tweeter[0] == 'p' or tweeter[1] == 'p':
+        if tweeter[0] == "p" or tweeter[1] == "p":
             if "#ECE4564T20" in tweeter:
-                tweeter = tweeter.replace('#ECE4564T20', '')
+                tweeter = tweeter.replace("#ECE4564T20", "")
             location = substr.substringByChar(tweeter, startChar=":", endChar="+")
             location = location[1:-1]
-            print('location:')
+            print("location:")
             print(location)
-            command = substr.substringByChar(tweeter, startChar="+", endChar=u'\u0020')
+            command = substr.substringByChar(tweeter, startChar="+", endChar=u"\u0020")
             command = command.rstrip()
             command = command[1:]
-            print('command:')
+            print("command:")
             print(command)
             message = re.findall(r'"([^"]*)"', tweeter)
             message = message[0]
-            print('message:')
+            print("message:")
             print(message)
 
-        elif tweeter[0] == 'c' or tweeter[1] == 'c':
+        elif tweeter[0] == "c" or tweeter[1] == "c":
             if "#ECE4564T20" in tweeter:
-                tweeter = tweeter.replace('#ECE4564T20', '')
-                command = substr.substringByChar(tweeter, startChar="+", endChar=u'\u0020')
+                tweeter = tweeter.replace("#ECE4564T20", "")
+                command = substr.substringByChar(
+                    tweeter, startChar="+", endChar=u"\u0020"
+                )
             else:
                 command = substr.substringByChar(tweeter, startChar="+")
 
@@ -68,12 +131,14 @@ class listener(tweepy.StreamListener):
     def on_error(self, status):
         print(status)
 
+
 #
 # The area of doubt
 #
 twitterStream = tweepy.Stream(auth, listener())
 twitterStream.filter(track=["#ECE4564T20"])
 # End Twitter Section
+
 
 def fetch_ip():
     return (
@@ -94,8 +159,6 @@ def fetch_ip():
     )[0]
 
 
-
-
 node = sys.argv[2]
 username = "Honaker"
 password = "buse"
@@ -109,7 +172,7 @@ channel = connection.channel()
 channel.exchange_declare(exchange="Squires", exchange_type="direct")
 channel.exchange_declare(exchange="Goodwin", exchange_type="direct")
 channel.exchange_declare(exchange="Library", exchange_type="direct")
-channel.exchange_declare(exchange="Commands", exchange_type="direct") 
+channel.exchange_declare(exchange="Commands", exchange_type="direct")
 
 message = "info:Hello World!"
 channel.basic_publish(exchange="Squires", routing_key="Food", body=message)
@@ -117,8 +180,8 @@ channel.basic_publish(exchange="Goodwin", routing_key="Goodwin", body=message)
 channel.basic_publish(exchange="Library", routing_key="Library", body=message)
 
 
-channel.queue_declare(queue='SendtoCapt')
-channel.queue_bind(exchange='Commands', queue='SendtoCapt', routing_key='SendtoCapt')
+channel.queue_declare(queue="SendtoCapt")
+channel.queue_bind(exchange="Commands", queue="SendtoCapt", routing_key="SendtoCapt")
 
 
 # connection.close()
@@ -235,10 +298,11 @@ print(
 )
 Checkpoint += 1
 
+
 def callback1(ch, method, properties, body):
     print("cold")
 
-channel.basic_consume(callback1, queue='SendtoCapt', no_ack=True)
+
+channel.basic_consume(callback1, queue="SendtoCapt", no_ack=True)
 
 channel.start_consuming()
-
